fe6341a079a56eaec76b1b26763631b31e2dfb9c
keko5342 <kekosute@gmail.com>
2019-03-07 13:07:18 +0900

    Fixing IllustTab

--- modules/Window.py
+++ modules/Window.py
@@ -17,13 +17,33 @@ class Window:
 
     def returnRoot(self, component):
         self.root = component.returnComponent(self.root)
+
+    # ListBox is Selected
+    def userClickedListBox(self, event):
+        selectUser = self.userTab.scrollListBox.get(self.userTab.scrollListBox.nearest(event.y))
+        print("User Clicked {}".format(selectUser))
+        imageUrl = self.userTab.twitterInfo.getImageURL(selectUser)
+        self.illustTab.downloadFourImage(imageUrl, self.monitorWidthQuoted)
+        '''
+        #if lstNmeCmbBox.get() != "Follow":
+        chkLstMember(selectUser)
+        getImgURL(selectUser)
+        setImage()
+        sctUser.clear()
+        sctUser.append(selectUser)
+        '''
     
     def __init__(self, monitorInfo, api):
         self.monitorInfo = monitorInfo
-        monitorHeight = self.monitorInfo.returnMonitorHeight()
-        monitorWidthHarfed = self.monitorInfo.returnMonitorWidth(2)
-        monitorWidthQuoted = self.monitorInfo.returnMonitorWidth(4)
+        self.monitorHeight = self.monitorInfo.returnMonitorHeight()
+        self.monitorWidthHarfed = self.monitorInfo.returnMonitorWidth(2)
+        self.monitorWidthQuoted = self.monitorInfo.returnMonitorWidth(4)
         self.setup()
+
+        # Create Sub Window
         self.userTab = UserTab.UserTab(master=self.root, width=monitorWidthQuoted, height=monitorHeight, api=api)
         self.illustTab = IllustTab.IllustTab(master=self.root, width=monitorWidthHarfed, height=monitorHeight)
         self.manageTab = ManageTab.ManageTab(master=self.root, width=monitorWidthQuoted, height=monitorHeight)
+
+        # Add Event
+        self.userTab.scrollListBox.bind("<Button-1>", self.userClickedListBox)
